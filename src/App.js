d350fcc24de10593a8bc46ff8527de89ad20a4ee
Gisela-Gentile <gisla.gentile@municipiodetandil.edu.ar>
2023-03-08 19:50:40 -0300

    Gisela Card

--- src/App.js
+++ src/App.js
@@ -18,6 +18,13 @@ function App() {
                   <p>Tecnologias: Git, React, Javascript...</p>
                </div>
             </div>
+            <div className="card">
+               <h3>Gisela Gentile</h3>
+               <div>
+                  <p>Alumna de FIP en CEPIT</p>
+                  <p>Tecnologias: Git, React, Javascript...</p>
+               </div>
+             </div>
          </header>
       </div>
    );
