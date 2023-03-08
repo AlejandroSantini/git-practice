6b52c1889e6f0176f09ea046ead2c7f6da92d147
Brunolmb <bruno.mancioni@municipiodetandil.edu.ar>
2023-03-08 19:33:10 -0300

    agregada card bruno

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ function App() {
                   <p>Descripcion</p>
                </div>
             </div>
+            <div className="card">
+               <h3>Bruno Leonel Mancioni Boggon</h3>
+               <div>
+                  <p>Alumno CEPIT</p>
+                  <p>Cursando desde 2022</p>
+               </div>
+            </div>
          </header>
       </div>
    );
